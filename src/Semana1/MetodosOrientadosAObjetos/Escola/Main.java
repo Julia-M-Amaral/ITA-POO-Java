b91a44a82ef8651dbf5571623a1181c4653a8ce8
Julia <JuliaAmaralSantos@outlook.com>
2024-02-08 22:19:58 -0300

    criação de métodos orientados a objetos nA classe aluno que verifica a media e se passou de ano.

---
+++ src/Semana1/MetodosOrientadosAObjetos/Escola/Main.java
@@ -0,0 +1,14 @@
+package Semana1.MetodosOrientadosAObjetos.Escola;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        a1.bim1 = 70;
+        a1.bim2 = 30;
+        a1.bim3 = 100;
+        a1.bim4 = 90;
+
+        System.out.println(a1.media());
+        System.out.println(a1.passouDeAno());
+    }
+}
