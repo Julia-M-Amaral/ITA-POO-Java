c3057c993fe5ea3d25f08f0e1dbf7944ac9a93c5
Julia <JuliaAmaralSantos@outlook.com>
2024-02-09 23:05:52 -0300

    adicionei um construtor para adicionar as notas do bimestre assim que criar o objeto

--- src/Semana2/MetodosOrientadosAObjetos/Escola/Aluno.java
+++ src/Semana2/MetodosOrientadosAObjetos/Escola/Aluno.java
@@ -7,12 +7,19 @@ public class Aluno {
     int bim3;
     int bim4;
 
+    public Aluno(int bim1, int bim2, int bim3, int bim4) {
+        this.bim1 = bim1;
+        this.bim2 = bim2;
+        this.bim3 = bim3;
+        this.bim4 = bim4;
+    }
+
     public int media(){
         int total = 0;
-        total += bim1;
-        total += bim2;
-        total += bim3;
-        total += bim4;
+        total += this.bim1;
+        total += this.bim2;
+        total += this.bim3;
+        total += this.bim4;
         return total / 4;
     }
 
--- src/Semana2/MetodosOrientadosAObjetos/Escola/Main.java
+++ src/Semana2/MetodosOrientadosAObjetos/Escola/Main.java
@@ -2,11 +2,7 @@ package Semana2.MetodosOrientadosAObjetos.Escola;
 
 public class Main {
     public static void main(String[] args) {
-        Aluno a1 = new Aluno();
-        a1.bim1 = 70;
-        a1.bim2 = 30;
-        a1.bim3 = 100;
-        a1.bim4 = 90;
+        Aluno a1 = new Aluno(70,30,100,90);
 
         System.out.println(a1.media());
         System.out.println(a1.passouDeAno());
